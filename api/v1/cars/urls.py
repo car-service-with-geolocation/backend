0d3d1989f74c77725d851b5136e728bcff2d2bdc
Pe4enkaaa5 <pe4enkaaa5@yandex.ru>
2023-09-19 17:55:50 +0300

    jobs add

--- api/v1/cars/urls.py
+++ api/v1/cars/urls.py
@@ -5,8 +5,8 @@ from rest_framework.routers import DefaultRouter
 from .views import TransportList, TransportDetail, CarsList, CarsDetail
 
 urlpatterns = [
-    path('transports/', TransportList.as_view(), name='transport-list'),
-    path('transports/<int:pk>/', TransportDetail.as_view(), name='transport-detail'),
-    path('cars/', CarsList.as_view(), name='car-list'),
-    path('cars/<int:pk>/', CarsDetail.as_view(), name='car-detail'),
+    path('', TransportList.as_view(), name='transport-list'),
+    path('<int:pk>/', TransportDetail.as_view(), name='transport-detail'),
+    path('', CarsList.as_view(), name='car-list'),
+    path('<int:pk>/', CarsDetail.as_view(), name='car-detail'),
 ]
--- api/v1/jobs/urls.py
+++ api/v1/jobs/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from .views import JobsList, JobsDetail
 
 urlpatterns = [
-    path('jobs/', JobsList.as_view(), name='jobs-list'),
-    path('jobs/<int:pk>/', JobsDetail.as_view(), name='jobs-detail'),
+    path('', JobsList.as_view(), name='jobs-list'),
+    path('<int:pk>/', JobsDetail.as_view(), name='jobs-detail'),
 ]

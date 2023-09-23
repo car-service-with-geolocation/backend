80223b00222140ebb75de105c468d9bc9faf221b
Pe4enka5 <pe4enkaaa5@yandex.ru>
2023-09-23 17:57:21 +0300

    CORS

--- car_service/settings.py
+++ car_service/settings.py
@@ -33,17 +33,22 @@ INSTALLED_APPS = [
     'cars',
     'core.apps.CoreConfig',
     'api.apps.ApiConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_URLS_REGEX = r'^/api/.*$'
 
 ROOT_URLCONF = 'car_service.urls'
 

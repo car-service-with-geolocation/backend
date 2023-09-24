85ffb4832b04ee806b680a89d0f17986b53caa4a
Pe4enka5 <pe4enkaaa5@yandex.ru>
2023-09-24 23:46:57 +0300

    image v/03

--- car_service/urls.py
+++ car_service/urls.py
@@ -6,6 +6,7 @@ from drf_yasg import openapi
 from django.conf.urls.static import static
 from django.conf.urls import url
 from django.conf import settings
+from django.views.static import serve
 
 from rest_framework.routers import DefaultRouter
 
@@ -42,3 +43,8 @@ urlpatterns += [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root': settings.MEDIA_ROOT}),
+]

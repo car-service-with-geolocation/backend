19bb41a5175c156744316a9dc5f5b372b181e8aa
Пашовкин Андрей <117083377+apashovkin48@users.noreply.github.com>
2023-10-05 11:12:44 +0300

    Update urls.py

--- car_service/urls.py
+++ car_service/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_yasg import openapi

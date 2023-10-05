a202ac2a19ce822a63c6f9c111e1c4dc1b8fa261
Пашовкин Андрей <117083377+apashovkin48@users.noreply.github.com>
2023-10-05 11:51:46 +0300

    Update views.py

--- api/v1/autoservice/views.py
+++ api/v1/autoservice/views.py
@@ -19,6 +19,9 @@ class CompanyViewset(viewsets.ReadOnlyModelViewSet):
     serializer_class = CompanySerializer
     queryset = Company.objects.all()
 
+    def get_serializer_context(self):
+        return {'request': None}
+
 
 class AutoServiceViewSet(
     viewsets.GenericViewSet,
@@ -32,6 +35,9 @@ class AutoServiceViewSet(
 
     serializer_class = AutoServiceSerializer
     permission_classes = [AllowAny]
+
+    def get_serializer_context(self):
+        return {'request': None}
 
     def get_queryset(self):
         queryset = AutoService.objects.select_related(

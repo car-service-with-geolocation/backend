a106ff0c8de80c9f4e26cd7d720ed3df2cd5fcd7
Oleg <iam@smaginoleg.ru>
2023-10-23 21:08:40 +0300

    <fix>: рефакторинг email_backend

--- car_service/settings.py
+++ car_service/settings.py
@@ -155,8 +155,8 @@ else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = 'smtp.yandex.ru'
     EMAIL_PORT = 465
-    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER'),
-    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD'),
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
     EMAIL_USE_SSL = True
     DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 

2794815f422203017fb90824633ff185f9dcf440
Santa <sevassanta@gmail.com>
2023-11-19 18:40:29 +0700

    add descriptions to User, lot of information

--- api/v1/users/views.py
+++ api/v1/users/views.py
@@ -6,14 +6,118 @@ import requests
 
 from .serializers import CustomUserSerializer
 
+from drf_spectacular.utils import extend_schema, extend_schema_view
 
+
+@extend_schema(
+    tags = ["Пользователь"],
+    methods = ["POST", "GET", "PATCH", "DELETE", "PUT"],
+  #  description="API для управления списком заказов."
+)
+@extend_schema_view(
+    create = extend_schema(tags = ["Пользователь"],
+                         description = "Создание пользователя",
+                         summary = "Создание пользователя"),
+    list = extend_schema(tags = ["Пользователь"],
+                       description = "Получение списка пользователей",
+                       summary = "Получение списка пользователей"),
+    retrieve = extend_schema(tags = ["Пользователь"],
+                           description = "Получение информации о пользователе",
+                           summary = "Получение информации о пользователе"),
+    update = extend_schema(tags = ["Пользователь"],
+                         description = "Обновление информации о пользователе",
+                         summary = "Обновление информации о пользователе"),
+    partial_update = extend_schema(tags = ["Пользователь"],
+                                 description = "Частичное обновление информации о пользователе",
+                                 summary = "Частичное обновление информации о пользователе"),
+    destroy = extend_schema(tags = ["Пользователь"],
+                          description = "Удаление пользователя",
+                          summary = "Удаление пользователя"),
+    activation = extend_schema(tags = ["Пользователь"],
+                          description = "Активация пользователя по uid и token",
+                          summary = "Активация пользователя"),
+    # login=extend_schema(
+    #     tags=["Пользователь"],
+    #     methods=["POST"],
+    #     description="Запрос на получение токена доступа",
+    #     summary="Запрос на получение токена доступа"
+    # ),
+    # logout=extend_schema(
+    #     tags=["Пользователь"],
+    #     methods=["POST"],
+    #     description="Запрос на аннулирование токена доступа",
+    #     summary="Запрос на аннулирование токена доступа"
+    # ),
+    me = [
+        extend_schema(
+            tags = ["Пользователь"],
+            description = "Получение информации о текущем пользователе",
+            summary = "Получение информации о текущем пользователе",
+            methods = ["GET"],
+        ),
+        extend_schema(
+            tags = ["Пользователь"],
+            description = "Изменение информации о текущем пользователе",
+            summary = "Изменение информации о текущем пользователе",
+            methods = ["PUT"],
+        ),
+        extend_schema(
+            tags = ["Пользователь"],
+            description = "Частичное обновление информации о текущем пользователе",
+            summary = "Частичное обновление информации о текущем пользователе",
+            methods = ["PATCH"],
+        ),
+        extend_schema(
+            tags = ["Пользователь"],
+            description = "Удаление текущего пользователя",
+            summary = "Удаление текущего пользователя",
+            methods = ["DELETE"],
+        ),
+    ],
+    resend_activation = extend_schema(
+            tags = ["Пользователь"],
+            description = "Запрос на повторное отправление кода активации",
+            summary = "Повторная отправка кода активации",
+        ),
+    reset_password = extend_schema(
+            tags = ["Пользователь"],
+            description = "Запрос на сброс пароля пользователя",
+            summary = "Сброс пароля пользователя",
+        ),
+    reset_password_confirm = extend_schema(
+            tags = ["Пользователь"],
+            description = "Подтверждение сброса пароля пользователя",
+            summary = " Подтверждение сброса пароля",
+        ),
+    reset_username = extend_schema(
+            tags = ["Пользователь"],
+            description = "Запрос на смену имени пользователя",
+            summary = "Смена имени пользователя",
+        ),
+    reset_username_confirm = extend_schema(
+            tags = ["Пользователь"],
+            description = "Подтверждение смены имени пользователя",
+            summary = "Подтверждение смены имени пользователя",
+        ),
+    set_password = extend_schema(
+            tags = ["Пользователь"],
+            description = "Установка нового пароля пользователя",
+            summary = "Установка нового пароля",
+        ),
+    set_username = extend_schema(
+            tags = ["Пользователь"],
+            description = "Установка нового имени пользователя.",
+            summary = "Установка нового имени пользователя",
+        ),
+
+)
 class CustomUserViewSet(UserViewSet):
-    """
-    Вьюсет предоставляет весь функционал CRUD для модели CustomUser
-    Вьюсет наследует стандартный UserViewSet из Djoser с последующим
-    переопределением метода create для регистрации пользователей по номеру
-    телефона.
-    """
+
+    #Вьюсет предоставляет весь функционал CRUD для модели CustomUser
+    #Вьюсет наследует стандартный UserViewSet из Djoser с последующим
+    #переопределением метода create для регистрации пользователей по номеру
+    #телефона.
+
     serializer = CustomUserSerializer
 
     def create(self, request, *args, **kwargs):
@@ -37,15 +141,26 @@ class CustomUserViewSet(UserViewSet):
                         status=status.HTTP_400_BAD_REQUEST)
 
     # def perform_create(self, serializer):
-        # TO DO
+        # TODO
 
 
+@extend_schema(
+    tags=["Пользователь"],
+    methods=["GET", "POST", "DELETE"],
+)
+@extend_schema_view(
+    get=extend_schema(tags=["Пользователь"],
+                      description='''Эндпоинт используется для активации пользователя.
+                                    При переходе по этой ссылке пользователь будет автоматически активирован передайте
+                                    uid и token''',
+                      summary="Активация пользователя"),
+)
 class CustomUserActivation(APIView):
-    """
-    Вью-функция предназначена для автоматизации активации пользователя при
-    переходе по ссылке для подтверждения почты путем передачи параметров
-    GET-запроса в теле POST-запроса на стандартный url Djoser
-    """
+
+    # Вью-функция предназначена для автоматизации активации пользователя при
+    # переходе по ссылке для подтверждения почты путем передачи параметров
+    # GET-запроса в теле POST-запроса на стандартный url Djoser
+
     def get(self, request, uid, token):
         data = {
             "uid": uid,

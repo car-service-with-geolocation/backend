41c9983734a75eb84f1e5bd76eeca286d6c7c3e2
GeorgiyKuzora <rafale87@gmail.com>
2024-01-15 21:58:49 +0300

    fix: 116 Создает класс CustomCurrentUserSerializer
    Класс CustomCurrentUserSerializer служит для того чтобы разделить логику сериализации данных
    запросов залогиненных и не залогиненных пользователей.

--- api/v1/users/serializers.py
+++ api/v1/users/serializers.py
@@ -33,3 +33,35 @@ class CustomUserSerializer(UserSerializer):
         validated_data['password'] = make_password(
             validated_data['password'])
         return super(UserSerializer, self).update(instance, validated_data)
+
+
+class CustomCurrentUserSerializer(UserSerializer):
+    """
+    Сериализатор для модели пользователя CustomUser.
+    Когда пользователь аутентифицирован.
+    """
+    password = serializers.CharField(
+        style={"input_type": "password"},
+        write_only=True
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = ('id',
+                  'email',
+                  'first_name',
+                  'password',
+                  'phone_number',
+                  'date_joined'
+                  )
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(
+            validated_data['password'])
+        return super(UserSerializer, self).create(validated_data)
+
+    def update(self, instance, validated_data):
+        if validated_data.get('password', None) is not None:
+            validated_data['password'] = make_password(
+                validated_data['password'])
+        return super(UserSerializer, self).update(instance, validated_data)

2a5f72c42a12c5e3a6c09d6c5b0fb8778eecd5c0
Alexander Malshakov <89681115653@mail.ru>
2024-01-17 13:12:53 +0300

    Добавляет manage команду import_user

---
+++ core/management/commands/import_user.py
@@ -0,0 +1,32 @@
+import json
+import os
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+def process_file(name: str):
+    with open(
+        os.path.join(settings.BASE_DIR, 'static/data/', name),
+        'r',
+        encoding='utf-8'
+    ) as file:
+        return json.load(file)
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        data = process_file('user.json')
+        for email, value in data.items():
+            User.objects.get_or_create(
+                email=email,
+                password=value[0],
+                first_name=value[1],
+                phone_number=value[2]
+            )
+        print('-----Пользователи загружены!-----')

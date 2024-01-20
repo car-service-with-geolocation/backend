9f10c73a4bb93a50a670975e40226c4b250bb510
GeorgiyKuzora <rafale87@gmail.com>
2024-01-20 19:04:55 +0300

    refactor: formated orders.views with black and isort

--- api/v1/order/views.py
+++ api/v1/order/views.py
@@ -1,18 +1,22 @@
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+
 from order.models import Order
+
 from .serializers import OrderSerializer
-from drf_spectacular.utils import extend_schema, extend_schema_view
 
 
 @extend_schema(
     tags=["Заказы"],
     methods=["POST", "GET"],
-    description="API для управления списком заказов."
+    description="API для управления списком заказов.",
 )
 @extend_schema_view(
-    get=extend_schema(description="Получить список всех заказов", summary="Получить данные"),
-    post=extend_schema(description="Создать новый заказ", summary="Отправить данные")
+    get=extend_schema(
+        description="Получить список всех заказов", summary="Получить данные"
+    ),
+    post=extend_schema(description="Создать новый заказ", summary="Отправить данные"),
 )
 class OrderListAPIView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
@@ -20,7 +24,7 @@ class OrderListAPIView(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        serializer.validated_data['user'] = self.request.user
+        serializer.validated_data["user"] = self.request.user
         serializer.save()
 
 
@@ -47,13 +51,24 @@ class CurrentUserOrderListAPIView(generics.ListCreateAPIView):
 @extend_schema(
     tags=["Заказы"],
     methods=["GET", "PUT", "PATCH", "DELETE"],
-    description="API для управления отдельным заказом."
+    description="API для управления отдельным заказом.",
 )
 @extend_schema_view(
-    get=extend_schema(description="Получить информацию о конкретном заказе по его ID", summary="Получить данные по id"),
-    put=extend_schema(description="Обновить информацию о конкретном заказе по его ID", summary="Обновить данные по id"),
-    patch=extend_schema(description="Изменить часть информации о заказе по его ID", summary="Изменить данные по id"),
-    delete=extend_schema(description="Удалить заказ по его ID", summary="Удалить данные по id")
+    get=extend_schema(
+        description="Получить информацию о конкретном заказе по его ID",
+        summary="Получить данные по id",
+    ),
+    put=extend_schema(
+        description="Обновить информацию о конкретном заказе по его ID",
+        summary="Обновить данные по id",
+    ),
+    patch=extend_schema(
+        description="Изменить часть информации о заказе по его ID",
+        summary="Изменить данные по id",
+    ),
+    delete=extend_schema(
+        description="Удалить заказ по его ID", summary="Удалить данные по id"
+    ),
 )
 class OrderDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Order.objects.all()

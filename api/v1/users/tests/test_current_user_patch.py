fff3cb2a4d7fcf7843624861a6ac3926b4dea4fd
GeorgiyKuzora <rafale87@gmail.com>
2024-01-20 12:04:52 +0300

    fix: 118 added test for CustomUserViewSet patch, put

--- api/v1/users/tests/test_current_user_patch.py
+++ api/v1/users/tests/test_current_user_patch.py
@@ -1,6 +1,10 @@
+import json
+
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory, force_authenticate
 
 from api.v1.users.serializers import CustomCurrentUserSerializer
+from api.v1.users.views import CustomUserViewSet
 from users.models import CustomUser
 
 test_user = {
@@ -17,9 +21,11 @@ class TestCurrentUserPatch(TestCase):
             email=test_user["email"],
             first_name=test_user["first_name"],
             password=test_user["password"],
+            phone_number=test_user["phone_number"],
         )
         self.user = CustomUser.objects.get(email=test_user["email"])
         self.serializer = CustomCurrentUserSerializer(partial=True)
+        self.factory = APIRequestFactory()
 
     def test_serializer_update_without_password(self) -> None:
         patch_data = {"email": "test_user@example.ru", "phone_number": "+79111111111"}
@@ -42,3 +48,92 @@ class TestCurrentUserPatch(TestCase):
 
         user_from_db = CustomUser.objects.get(email=test_user["email"])
         self.assertEqual(user_from_db.phone_number, patch_data["phone_number"])
+
+    def test_class_view_with_patch_request(self):
+        test_data = [
+            {
+                "email": test_user["email"],
+                "first_name": "changed",
+                "phone_number": "+79111111112",
+                "password": test_user["password"],
+            },
+            {
+                "email": test_user["email"],
+                "first_name": "changed",
+                "phone_number": "+79111111112",
+            },
+            {
+                "first_name": "changed",
+                "phone_number": "+79111111112",
+                "password": test_user["password"],
+            },
+            {
+                "email": "second_example@example.com",
+                "first_name": "changed",
+                "phone_number": "+79111111112",
+                "password": test_user["password"],
+            },
+            {
+                "email": test_user["email"],
+                "first_name": "changed",
+                "phone_number": "+79111111112",
+                "password": "dsa92-hap",
+            },
+            {
+                "email": "second_example@example.com",
+                "first_name": "changed",
+                "phone_number": "+79111111112",
+                "password": "dsa92-hap",
+            },
+        ]
+        for patch in test_data:
+            with self.subTest(msg=patch):
+                request = self.factory.patch(
+                    "/api/v1/auth/users/me/",
+                    json.dumps(patch),
+                    content_type="application/json",
+                )
+                force_authenticate(request, user=self.user)
+                view = CustomUserViewSet.as_view({"patch": "me"})
+                response = view(request)
+                self.assertEqual(response.status_code, 200)
+
+
+    def test_class_view_with_put_request(self):
+        test_data = [
+            {
+                "email": test_user["email"],
+                "first_name": "changed",
+                "phone_number": "+79111111112",
+                "password": test_user["password"],
+            },
+            {
+                "email": "second_example@example.com",
+                "first_name": "changed",
+                "phone_number": "+79111111112",
+                "password": test_user["password"],
+            },
+            {
+                "email": test_user["email"],
+                "first_name": "changed",
+                "phone_number": "+79111111112",
+                "password": "dsa92-hap",
+            },
+            {
+                "email": "second_example@example.com",
+                "first_name": "changed",
+                "phone_number": "+79111111112",
+                "password": "dsa92-hap",
+            },
+        ]
+        for put in test_data:
+            with self.subTest(msg=put):
+                request = self.factory.put(
+                    "/api/v1/auth/users/me/",
+                    json.dumps(put),
+                    content_type="application/json",
+                )
+                force_authenticate(request, user=self.user)
+                view = CustomUserViewSet.as_view({"put": "me"})
+                response = view(request)
+                self.assertEqual(response.status_code, 200)

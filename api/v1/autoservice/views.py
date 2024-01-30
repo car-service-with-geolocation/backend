4144132f17d06f331081a1c56339254f3278b0e6
Alexander Malshakov <89681115653@mail.ru>
2024-01-30 10:33:37 +0300

    Изменяет WorkingTime

--- api/v1/autoservice/views.py
+++ api/v1/autoservice/views.py
@@ -9,12 +9,12 @@ from django.db.models.functions import (
 )
 from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import get_object_or_404
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import filters, generics, mixins, viewsets
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 from autoservice.models import AutoService, Company, Job, Transport, Image
 from core.utils import is_float
-
 from .filters import TransportsFilter, JobsFilter
 from .serializers import (
     AutoServiceSerializer,
@@ -26,21 +26,19 @@ from .serializers import (
 )
 from .permissions import IsAuthorOrAdminReadOnly
 
-from drf_spectacular.utils import extend_schema, extend_schema_view
-
 
 @extend_schema(
-    tags = ["Автосервисы"],
-    methods = ["GET"],
+    tags=["Автосервисы"],
+    methods=["GET"],
 )
 @extend_schema_view(
-    get = extend_schema(description = "Получить список всех моделей автомобилей",
-                        summary = "Получить список моделей а/м ")
+    get=extend_schema(
+        description="Получить список всех моделей автомобилей",
+        summary="Получить список моделей а/м "
+    )
 )
 class TransportList(generics.ListAPIView):
-
-    # Вьюсет для чтения информации о компаниях по ремонту авто.
-
+    """ListAPIView для чтения информации о компаниях по ремонту авто."""
     queryset = Transport.objects.all()
     serializer_class = TransportsSerializer
     filter_backends = (filters.SearchFilter, DjangoFilterBackend)
@@ -49,56 +47,56 @@ class TransportList(generics.ListAPIView):
 
 
 @extend_schema(
-    tags = ["Автосервисы"],
-    methods = ["GET"],
+    tags=["Автосервисы"],
+    methods=["GET"],
 )
 @extend_schema_view(
-    get = extend_schema(description = "Получить информацию о модели автомобиля по id",
-                        summary = "Получить информацию а/м по id"))
-
+    get=extend_schema(
+        description="Получить информацию о модели автомобиля по id",
+        summary="Получить информацию а/м по id"
+    )
+)
 class TransportDetail(generics.RetrieveAPIView):
-
-    # Вьюсет для чтения информации о компаниях по ремонту авто.
-
+    """APIView для чтения информации о компаниях по ремонту авто."""
     queryset = Transport.objects.all()
     serializer_class = TransportsSerializer
 
 
 @extend_schema(
-    tags = ["Автосервисы"],
-    methods = ["GET"],
+    tags=["Автосервисы"],
+    methods=["GET"],
 )
 @extend_schema_view(
         list=extend_schema(
-            description = "Получить список компаний по ремонту авто",
-            summary = "Получить список компаний",
+            description="Получить список компаний по ремонту авто",
+            summary="Получить список компаний",
             tags=["Автосервисы"]
         ),
         retrieve=extend_schema(
-            description = "Получить информацию о компании по ремонту авто",
-            summary = "Получить информация о компании по id",
+            description="Получить информацию о компании по ремонту авто",
+            summary="Получить информация о компании по id",
             tags=["Автосервисы"]
         )
     )
 class CompanyViewset(viewsets.ReadOnlyModelViewSet):
-
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
 
+
 @extend_schema(
-    tags = ["Автосервисы"],
-    methods = ["GET"],
+    tags=["Автосервисы"],
+    methods=["GET"],
 )
 @extend_schema_view(
-        list = extend_schema(
-            summary = "Получить список автосервисов.",
-            description = "Получить список автосервисов , param: latitude",
-            tags = ["Автосервисы"]
+        list=extend_schema(
+            summary="Получить список автосервисов.",
+            description="Получить список автосервисов , param: latitude",
+            tags=["Автосервисы"]
         ),
-        retrieve = extend_schema(
-            summary = "Получить детали автосервиса по id",
-            description = "Получить детали автосервиса по id",
-            tags = ["Автосервисы"]
+        retrieve=extend_schema(
+            summary="Получить детали автосервиса по id",
+            description="Получить детали автосервиса по id",
+            tags=["Автосервисы"]
         )
     )
 class AutoServiceViewSet(
@@ -106,10 +104,9 @@ class AutoServiceViewSet(
     mixins.ListModelMixin,
     mixins.RetrieveModelMixin
 ):
-
-    #ViewSet для получения списка автосервисов
-    #param: latitude
-
+    """ViewSet для получения списка автосервисов.
+    param: latitude.
+    """
     serializer_class = AutoServiceSerializer
     permission_classes = [AllowAny]
 
@@ -137,63 +134,49 @@ class AutoServiceViewSet(
         ):
             lat = float(self.request.query_params['latitude'])
             lon = float(self.request.query_params['longitude'])
-            queryset = queryset.annotate(
-                distance=(
-                        2 * 6371
-                        * ASin(Sqrt(
-                    Power(Sin((
-                                      Radians(F('geolocation__latitude')) - Radians(lat)
-                              ) / 2), 2)
-                    + Cos(Radians(lat))
-                    * Cos(Radians(F('geolocation__latitude')))
-                    * Power(Sin((
-                                        Radians(F('geolocation__longitude')) - Radians(lon)
-                                ) / 2), 2)
-                ))
-                )
-            ).order_by('distance', '-rating')
+            # todo Вынести логику подсчета в отдельную функцию, убрать ее из views.py
+            queryset = queryset.annotate(distance=(2 * 6371 * ASin(Sqrt(Power(Sin((Radians(F('geolocation__latitude')) - Radians(lat)) / 2), 2) + Cos(Radians(lat)) * Cos(Radians(F('geolocation__latitude'))) * Power(Sin((Radians(F('geolocation__longitude')) - Radians(lon)) / 2), 2))))).order_by('distance', '-rating')
         return queryset
 
+
 @extend_schema(
-    tags = ["Отзывы"],
-    methods = ["GET", "POST", "PATCH", "DELETE"],
+    tags=["Отзывы"],
+    methods=["GET", "POST", "PATCH", "DELETE"],
 )
 @extend_schema_view(
-    list = extend_schema(
-        summary = "Получить отзывы об автосервисе",
-        description = "Получить список отзывыв об автосервисе необходимо указать id автосервиса",
-        tags = ["Отзывы"]
+    list=extend_schema(
+        summary="Получить отзывы об автосервисе",
+        description="Получить список отзывыв об автосервисе необходимо указать id автосервиса",
+        tags=["Отзывы"]
     ),
-    create = extend_schema(
-        summary = "Создать отзыв об автосервисе",
-        description = "Создать отзыв об автосервисе необходимо указать id автосервиса",
-        tags = ["Отзывы"]
+    create=extend_schema(
+        summary="Создать отзыв об автосервисе",
+        description="Создать отзыв об автосервисе необходимо указать id автосервиса",
+        tags=["Отзывы"]
     ),
-    retrieve = extend_schema(
-        summary = "Получить детали отзыва об автосервисе",
-        description = "Получить детали отзыва об автосервисе необходимо указать id автосервиса и id отзыва",
-        tags = ["Отзывы"]
+    retrieve=extend_schema(
+        summary="Получить детали отзыва об автосервисе",
+        description="Получить детали отзыва об автосервисе необходимо указать id автосервиса и id отзыва",
+        tags=["Отзывы"]
     ),
-    update = extend_schema(
-        summary = "Изменить отзыв об автосервисе ",
-        description = "Изменить отзыв об автосервисе необходимо указать id автосервиса и id отзыва",
-        tags = ["Отзывы"]
+    update=extend_schema(
+        summary="Изменить отзыв об автосервисе ",
+        description="Изменить отзыв об автосервисе необходимо указать id автосервиса и id отзыва",
+        tags=["Отзывы"]
     ),
-    partial_update = extend_schema(
-        summary = "Изменить отзыв об автосервисе.",
-        description = "Изменить отзыв об автосервисе необходимо указать id автосервиса и id отзыва",
-        tags = ["Отзывы"]
+    partial_update=extend_schema(
+        summary="Изменить отзыв об автосервисе.",
+        description="Изменить отзыв об автосервисе необходимо указать id автосервиса и id отзыва",
+        tags=["Отзывы"]
     ),
-    destroy = extend_schema(
-        summary = "Удалить отзыв об автосервисе",
-        description = "Удалить отзыв об автосервисе необходимо указать id автосервиса и id отзыва",
-        tags = ["Отзывы"]
+    destroy=extend_schema(
+        summary="Удалить отзыв об автосервисе",
+        description="Удалить отзыв об автосервисе необходимо указать id автосервиса и id отзыва",
+        tags=["Отзывы"]
     )
 )
 class FeedbackViewSet(viewsets.ModelViewSet):
-
-    # ViewSet для модели отзывов Feedback.
-
+    """ViewSet для модели отзывов Feedback."""
     serializer_class = FeedbackSerializer
     http_method_names = ('get', 'post', 'patch', 'delete')
     permission_classes = [IsAuthorOrAdminReadOnly]
@@ -218,20 +201,15 @@ class FeedbackViewSet(viewsets.ModelViewSet):
             feedback.images.add(image)
 
 
-class JobsList(generics.ListAPIView):
-    queryset = Job.objects.all()
-    serializer_class = JobsSerializer
-    filter_backends = (filters.SearchFilter, DjangoFilterBackend)
-    filterset_class = JobsFilter
-    search_fields = ('name', )
-
-
 @extend_schema(
-    tags = ["Работы"],
-    methods = ["GET"],
+    tags=["Работы"],
+    methods=["GET"],
 )
 @extend_schema_view(
-    get=extend_schema(description = "Получить список всех работ", summary = "Получить все данные")
+    get=extend_schema(
+        description="Получить список всех работ",
+        summary="Получить все данные"
+    )
 )
 class JobsList(generics.ListAPIView):
     queryset = Job.objects.all()
@@ -239,13 +217,17 @@ class JobsList(generics.ListAPIView):
     filter_backends = (filters.SearchFilter, DjangoFilterBackend)
     filterset_class = JobsFilter
     search_fields = ('name', )
+
 
 @extend_schema(
     tags=["Работы"],
     methods=["GET"],
 )
 @extend_schema_view(
-    get=extend_schema(description = "Получить подробную информацию о конкретной работе", summary = "Получить данные по id")
+    get=extend_schema(
+        description="Получить подробную информацию о конкретной работе",
+        summary="Получить данные по id"
+        )
 )
 class JobsDetail(generics.RetrieveAPIView):
     queryset = Job.objects.all()

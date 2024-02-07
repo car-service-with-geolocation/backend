610638baf80d76db18e533699ecc0637875f352b
Alexander Malshakov <89681115653@mail.ru>
2024-02-07 18:55:46 +0300

    blackformatter users/views.py

--- api/v1/users/views.py
+++ api/v1/users/views.py
@@ -11,94 +11,107 @@ from drf_spectacular.utils import extend_schema, extend_schema_view
 
 
 @extend_schema(
-    tags = ["Пользователь"],
-    methods = ["POST", "GET", "PATCH", "DELETE", "PUT"],
-  #  description="API для управления списком заказов."
+    tags=["Пользователь"],
+    methods=["POST", "GET", "PATCH", "DELETE", "PUT"],
+    #  description="API для управления списком заказов."
 )
 @extend_schema_view(
-    create = extend_schema(tags = ["Пользователь"],
-                         description = "Создание пользователя",
-                         summary = "Создание пользователя"),
-    list = extend_schema(tags = ["Пользователь"],
-                       description = "Получение списка пользователей",
-                       summary = "Получение списка пользователей"),
-    retrieve = extend_schema(tags = ["Пользователь"],
-                           description = "Получение информации о пользователе",
-                           summary = "Получение информации о пользователе"),
-    update = extend_schema(tags = ["Пользователь"],
-                         description = "Обновление информации о пользователе",
-                         summary = "Обновление информации о пользователе"),
-    partial_update = extend_schema(tags = ["Пользователь"],
-                                 description = "Частичное обновление информации о пользователе",
-                                 summary = "Частичное обновление информации о пользователе"),
-    destroy = extend_schema(tags = ["Пользователь"],
-                          description = "Удаление пользователя",
-                          summary = "Удаление пользователя"),
-    activation = extend_schema(tags = ["Пользователь"],
-                          description = "Активация пользователя по uid и token",
-                          summary = "Активация пользователя"),
-    me = [
+    create=extend_schema(
+        tags=["Пользователь"],
+        description="Создание пользователя",
+        summary="Создание пользователя",
+    ),
+    list=extend_schema(
+        tags=["Пользователь"],
+        description="Получение списка пользователей",
+        summary="Получение списка пользователей",
+    ),
+    retrieve=extend_schema(
+        tags=["Пользователь"],
+        description="Получение информации о пользователе",
+        summary="Получение информации о пользователе",
+    ),
+    update=extend_schema(
+        tags=["Пользователь"],
+        description="Обновление информации о пользователе",
+        summary="Обновление информации о пользователе",
+    ),
+    partial_update=extend_schema(
+        tags=["Пользователь"],
+        description="Частичное обновление информации о пользователе",
+        summary="Частичное обновление информации о пользователе",
+    ),
+    destroy=extend_schema(
+        tags=["Пользователь"],
+        description="Удаление пользователя",
+        summary="Удаление пользователя",
+    ),
+    activation=extend_schema(
+        tags=["Пользователь"],
+        description="Активация пользователя по uid и token",
+        summary="Активация пользователя",
+    ),
+    me=[
         extend_schema(
-            tags = ["Пользователь"],
-            description = "Получение информации о текущем пользователе",
-            summary = "Получение информации о текущем пользователе",
-            methods = ["GET"],
+            tags=["Пользователь"],
+            description="Получение информации о текущем пользователе",
+            summary="Получение информации о текущем пользователе",
+            methods=["GET"],
         ),
         extend_schema(
-            tags = ["Пользователь"],
-            description = "Изменение информации о текущем пользователе",
-            summary = "Изменение информации о текущем пользователе",
-            methods = ["PUT"],
+            tags=["Пользователь"],
+            description="Изменение информации о текущем пользователе",
+            summary="Изменение информации о текущем пользователе",
+            methods=["PUT"],
         ),
         extend_schema(
-            tags = ["Пользователь"],
-            description = "Частичное обновление информации о текущем пользователе",
-            summary = "Частичное обновление информации о текущем пользователе",
-            methods = ["PATCH"],
+            tags=["Пользователь"],
+            description="Частичное обновление информации о текущем пользователе",
+            summary="Частичное обновление информации о текущем пользователе",
+            methods=["PATCH"],
         ),
         extend_schema(
-            tags = ["Пользователь"],
-            description = "Удаление текущего пользователя",
-            summary = "Удаление текущего пользователя",
-            methods = ["DELETE"],
+            tags=["Пользователь"],
+            description="Удаление текущего пользователя",
+            summary="Удаление текущего пользователя",
+            methods=["DELETE"],
         ),
     ],
-    resend_activation = extend_schema(
-            tags = ["Пользователь"],
-            description = "Запрос на повторное отправление кода активации",
-            summary = "Повторная отправка кода активации",
-        ),
-    reset_password = extend_schema(
-            tags = ["Пользователь"],
-            description = "Запрос на сброс пароля пользователя",
-            summary = "Сброс пароля пользователя",
-        ),
-    reset_password_confirm = extend_schema(
-            tags = ["Пользователь"],
-            description = "Подтверждение сброса пароля пользователя",
-            summary = " Подтверждение сброса пароля",
-        ),
-    reset_username = extend_schema(
-            tags = ["Пользователь"],
-            description = "Запрос на смену имени пользователя",
-            summary = "Смена имени пользователя",
-        ),
-    reset_username_confirm = extend_schema(
-            tags = ["Пользователь"],
-            description = "Подтверждение смены имени пользователя",
-            summary = "Подтверждение смены имени пользователя",
-        ),
-    set_password = extend_schema(
-            tags = ["Пользователь"],
-            description = "Установка нового пароля пользователя",
-            summary = "Установка нового пароля",
-        ),
-    set_username = extend_schema(
-            tags = ["Пользователь"],
-            description = "Установка нового имени пользователя.",
-            summary = "Установка нового имени пользователя",
-        ),
-
+    resend_activation=extend_schema(
+        tags=["Пользователь"],
+        description="Запрос на повторное отправление кода активации",
+        summary="Повторная отправка кода активации",
+    ),
+    reset_password=extend_schema(
+        tags=["Пользователь"],
+        description="Запрос на сброс пароля пользователя",
+        summary="Сброс пароля пользователя",
+    ),
+    reset_password_confirm=extend_schema(
+        tags=["Пользователь"],
+        description="Подтверждение сброса пароля пользователя",
+        summary=" Подтверждение сброса пароля",
+    ),
+    reset_username=extend_schema(
+        tags=["Пользователь"],
+        description="Запрос на смену имени пользователя",
+        summary="Смена имени пользователя",
+    ),
+    reset_username_confirm=extend_schema(
+        tags=["Пользователь"],
+        description="Подтверждение смены имени пользователя",
+        summary="Подтверждение смены имени пользователя",
+    ),
+    set_password=extend_schema(
+        tags=["Пользователь"],
+        description="Установка нового пароля пользователя",
+        summary="Установка нового пароля",
+    ),
+    set_username=extend_schema(
+        tags=["Пользователь"],
+        description="Установка нового имени пользователя.",
+        summary="Установка нового имени пользователя",
+    ),
 )
 class CustomUserViewSet(UserViewSet):
     """
@@ -116,7 +129,8 @@ class CustomUserViewSet(UserViewSet):
             return super().create(request, *args, **kwargs)
 
         return Response(
-            {"error": "Invalid registration method"}, status=status.HTTP_400_BAD_REQUEST
+            {"error": "Invalid registration method"},
+            status=status.HTTP_400_BAD_REQUEST,
         )
 
 

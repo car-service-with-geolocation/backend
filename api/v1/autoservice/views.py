8b5366d1db2fc3cf52fe5e66bd1aacc1d6f0dff2
GeorgiyKuzora <rafale87@gmail.com>
2024-02-11 17:24:16 +0300

    refactor: Format with black

--- api/v1/autoservice/views.py
+++ api/v1/autoservice/views.py
@@ -1,36 +1,30 @@
-from django.db.models import F, Avg, Count
-from django.db.models.functions import (
-    ASin,
-    Cos,
-    Power,
-    Radians,
-    Sin,
-    Sqrt
-)
-from django_filters.rest_framework import DjangoFilterBackend
 from copy import copy
 from django.contrib.auth.models import Group
+from django.db.models import Avg, Count, F
+from django.db.models.functions import ASin, Cos, Power, Radians, Sin, Sqrt
 from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import filters, generics, mixins, status, viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from api.v1.users.serializers import CustomUserSerializer
 
-from autoservice.models import AutoService, Company, Job, Transport, Image
 from api.v1.users.views import CustomUserViewSet
+from autoservice.models import AutoService, Company, Image, Job, Transport
 from core.utils import is_float
-from .filters import TransportsFilter, JobsFilter
 from users.models import CustomUser
+
+from .filters import JobsFilter, TransportsFilter
+from .permissions import IsAuthorOrAdminReadOnly
 from .serializers import (
     AutoServiceSerializer,
     CompanySerializer,
     FeedbackSerializer,
+    JobsSerializer,
     ListAutoServiceSerializer,
     TransportsSerializer,
-    JobsSerializer
 )
-from .permissions import IsAuthorOrAdminReadOnly
 
 
 @extend_schema(
@@ -40,16 +34,17 @@ from .permissions import IsAuthorOrAdminReadOnly
 @extend_schema_view(
     get=extend_schema(
         description="Получить список всех моделей автомобилей",
-        summary="Получить список моделей а/м "
+        summary="Получить список моделей а/м ",
     )
 )
 class TransportList(generics.ListAPIView):
     """ListAPIView для чтения информации о компаниях по ремонту авто."""
+
     queryset = Transport.objects.all()
     serializer_class = TransportsSerializer
     filter_backends = (filters.SearchFilter, DjangoFilterBackend)
     filterset_class = TransportsFilter
-    search_fields = ('brand',)
+    search_fields = ("brand",)
 
 
 @extend_schema(
@@ -59,11 +54,12 @@ class TransportList(generics.ListAPIView):
 @extend_schema_view(
     get=extend_schema(
         description="Получить информацию о модели автомобиля по id",
-        summary="Получить информацию а/м по id"
+        summary="Получить информацию а/м по id",
     )
 )
 class TransportDetail(generics.RetrieveAPIView):
     """APIView для чтения информации о компаниях по ремонту авто."""
+
     queryset = Transport.objects.all()
     serializer_class = TransportsSerializer
 
@@ -73,17 +69,17 @@ class TransportDetail(generics.RetrieveAPIView):
     methods=["GET"],
 )
 @extend_schema_view(
-        list=extend_schema(
-            description="Получить список компаний по ремонту авто",
-            summary="Получить список компаний",
-            tags=["Автосервисы"]
-        ),
-        retrieve=extend_schema(
-            description="Получить информацию о компании по ремонту авто",
-            summary="Получить информация о компании по id",
-            tags=["Автосервисы"]
-        )
-    )
+    list=extend_schema(
+        description="Получить список компаний по ремонту авто",
+        summary="Получить список компаний",
+        tags=["Автосервисы"],
+    ),
+    retrieve=extend_schema(
+        description="Получить информацию о компании по ремонту авто",
+        summary="Получить информация о компании по id",
+        tags=["Автосервисы"],
+    ),
+)
 class CompanyViewset(viewsets.ReadOnlyModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
@@ -94,54 +90,76 @@ class CompanyViewset(viewsets.ReadOnlyModelViewSet):
     methods=["GET"],
 )
 @extend_schema_view(
-        list=extend_schema(
-            summary="Получить список автосервисов.",
-            description="Получить список автосервисов , param: latitude",
-            tags=["Автосервисы"]
-        ),
-        retrieve=extend_schema(
-            summary="Получить детали автосервиса по id",
-            description="Получить детали автосервиса по id",
-            tags=["Автосервисы"]
-        )
-    )
+    list=extend_schema(
+        summary="Получить список автосервисов.",
+        description="Получить список автосервисов , param: latitude",
+        tags=["Автосервисы"],
+    ),
+    retrieve=extend_schema(
+        summary="Получить детали автосервиса по id",
+        description="Получить детали автосервиса по id",
+        tags=["Автосервисы"],
+    ),
+)
 class AutoServiceViewSet(
-    viewsets.GenericViewSet,
-    mixins.ListModelMixin,
-    mixins.RetrieveModelMixin
+    viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin
 ):
     """ViewSet для получения списка автосервисов.
     param: latitude.
     """
+
     serializer_class = AutoServiceSerializer
     permission_classes = [AllowAny]
 
     def get_serializer_class(self):
-        if self.action in 'list':
+        if self.action in "list":
             return ListAutoServiceSerializer
         return AutoServiceSerializer
 
     def get_queryset(self):
         queryset = (
-            AutoService
-            .objects
-            .select_related("geolocation")
-            .annotate(
-                rating=Avg('feedback__score'),
-                votes=Count('feedback__score')
-            )
-            .order_by('-rating')
+            AutoService.objects.select_related("geolocation")
+            .annotate(rating=Avg("feedback__score"), votes=Count("feedback__score"))
+            .order_by("-rating")
         )
         if (
-                'latitude' in self.request.query_params
-                and 'longitude' in self.request.query_params
-                and is_float(self.request.query_params['latitude'])
-                and is_float(self.request.query_params['longitude'])
+            "latitude" in self.request.query_params
+            and "longitude" in self.request.query_params
+            and is_float(self.request.query_params["latitude"])
+            and is_float(self.request.query_params["longitude"])
         ):
-            lat = float(self.request.query_params['latitude'])
-            lon = float(self.request.query_params['longitude'])
+            lat = float(self.request.query_params["latitude"])
+            lon = float(self.request.query_params["longitude"])
             # todo Вынести логику подсчета в отдельную функцию, убрать ее из views.py
-            queryset = queryset.annotate(distance=(2 * 6371 * ASin(Sqrt(Power(Sin((Radians(F('geolocation__latitude')) - Radians(lat)) / 2), 2) + Cos(Radians(lat)) * Cos(Radians(F('geolocation__latitude'))) * Power(Sin((Radians(F('geolocation__longitude')) - Radians(lon)) / 2), 2))))).order_by('distance', '-rating')
+            queryset = queryset.annotate(
+                distance=(
+                    2
+                    * 6371
+                    * ASin(
+                        Sqrt(
+                            Power(
+                                Sin(
+                                    (Radians(F("geolocation__latitude")) - Radians(lat))
+                                    / 2
+                                ),
+                                2,
+                            )
+                            + Cos(Radians(lat))
+                            * Cos(Radians(F("geolocation__latitude")))
+                            * Power(
+                                Sin(
+                                    (
+                                        Radians(F("geolocation__longitude"))
+                                        - Radians(lon)
+                                    )
+                                    / 2
+                                ),
+                                2,
+                            )
+                        )
+                    )
+                )
+            ).order_by("distance", "-rating")
         return queryset
 
 
@@ -153,56 +171,53 @@ class AutoServiceViewSet(
     list=extend_schema(
         summary="Получить отзывы об автосервисе",
         description="Получить список отзывыв об автосервисе необходимо указать id автосервиса",
-        tags=["Отзывы"]
+        tags=["Отзывы"],
     ),
     create=extend_schema(
         summary="Создать отзыв об автосервисе",
         description="Создать отзыв об автосервисе необходимо указать id автосервиса",
-        tags=["Отзывы"]
+        tags=["Отзывы"],
     ),
     retrieve=extend_schema(
         summary="Получить детали отзыва об автосервисе",
         description="Получить детали отзыва об автосервисе необходимо указать id автосервиса и id отзыва",
-        tags=["Отзывы"]
+        tags=["Отзывы"],
     ),
     update=extend_schema(
         summary="Изменить отзыв об автосервисе ",
         description="Изменить отзыв об автосервисе необходимо указать id автосервиса и id отзыва",
-        tags=["Отзывы"]
+        tags=["Отзывы"],
     ),
     partial_update=extend_schema(
         summary="Изменить отзыв об автосервисе.",
         description="Изменить отзыв об автосервисе необходимо указать id автосервиса и id отзыва",
-        tags=["Отзывы"]
+        tags=["Отзывы"],
     ),
     destroy=extend_schema(
         summary="Удалить отзыв об автосервисе",
         description="Удалить отзыв об автосервисе необходимо указать id автосервиса и id отзыва",
-        tags=["Отзывы"]
-    )
+        tags=["Отзывы"],
+    ),
 )
 class FeedbackViewSet(viewsets.ModelViewSet):
     """ViewSet для модели отзывов Feedback."""
+
     serializer_class = FeedbackSerializer
-    http_method_names = ('get', 'post', 'patch', 'delete')
+    http_method_names = ("get", "post", "patch", "delete")
     permission_classes = [IsAuthorOrAdminReadOnly]
 
     def get_autoservice(self):
-        return get_object_or_404(
-            AutoService,
-            pk=self.kwargs.get('autoservice_id')
-        )
+        return get_object_or_404(AutoService, pk=self.kwargs.get("autoservice_id"))
 
     def get_queryset(self):
         return self.get_autoservice().feedback.all()
 
     def perform_create(self, serializer):
         feedback = serializer.save(
-            author=self.request.user,
-            autoservice=self.get_autoservice()
+            author=self.request.user, autoservice=self.get_autoservice()
         )
 
-        for file in self.request.FILES.getlist('images'):
+        for file in self.request.FILES.getlist("images"):
             image = Image.objects.create(image=file)
             feedback.images.add(image)
 
@@ -213,8 +228,7 @@ class FeedbackViewSet(viewsets.ModelViewSet):
 )
 @extend_schema_view(
     get=extend_schema(
-        description="Получить список всех работ",
-        summary="Получить все данные"
+        description="Получить список всех работ", summary="Получить все данные"
     )
 )
 class JobsList(generics.ListAPIView):
@@ -222,7 +236,7 @@ class JobsList(generics.ListAPIView):
     serializer_class = JobsSerializer
     filter_backends = (filters.SearchFilter, DjangoFilterBackend)
     filterset_class = JobsFilter
-    search_fields = ('name', )
+    search_fields = ("name",)
 
 
 @extend_schema(
@@ -232,8 +246,8 @@ class JobsList(generics.ListAPIView):
 @extend_schema_view(
     get=extend_schema(
         description="Получить подробную информацию о конкретной работе",
-        summary="Получить данные по id"
-        )
+        summary="Получить данные по id",
+    )
 )
 class JobsDetail(generics.RetrieveAPIView):
     queryset = Job.objects.all()

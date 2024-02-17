6f73924a801003e5e6cba566a5ba860719aea003
GeorgiyKuzora <rafale87@gmail.com>
2024-02-17 12:36:15 +0300

    feat:145 add custom validator method
    Add custom validator method /core/validators.py validate_all_isdigit.

---
+++ core/validators.py
@@ -0,0 +1,10 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+
+def validate_all_isdigit(value: str):
+    if not all((char.isdigit() for char in value)):
+        raise ValidationError(
+            _("%(value) contains not digit characters"),
+            params={"value": value},
+        )

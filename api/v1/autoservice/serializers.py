9001cd6f966e111f11a995088f0a7706779a1105
GeorgiyKuzora <rafale87@gmail.com>
2024-02-17 12:52:11 +0300

    refactor: format with black api/v1/autoservice/serializers.py

--- api/v1/autoservice/serializers.py
+++ api/v1/autoservice/serializers.py
@@ -14,7 +14,7 @@ from autoservice.models import (
     Image,
     Job,
     Transport,
-    WorkingTime
+    WorkingTime,
 )
 
 
@@ -23,7 +23,7 @@ class TransportsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Transport
-        fields = ('id', 'brand')
+        fields = ("id", "brand")
 
 
 class CompanySerializer(serializers.ModelSerializer):
@@ -32,12 +32,12 @@ class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
         fields = [
-            'id',
-            'title',
-            'description',
-            'logo',
-            'legal_address',
-            'owner',
+            "id",
+            "title",
+            "description",
+            "logo",
+            "legal_address",
+            "owner",
         ]
 
 
@@ -59,10 +59,7 @@ class CompanyShortSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Company
-        fields = [
-            'title',
-            'logo'
-        ]
+        fields = ["title", "logo"]
 
 
 class GeolocationAutoServiceSerializer(serializers.ModelSerializer):
@@ -70,20 +67,21 @@ class GeolocationAutoServiceSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = GeolocationAutoService
-        fields = ['latitude', 'longitude']
+        fields = ["latitude", "longitude"]
 
 
 class AutoserviceJobSerializer(serializers.ModelSerializer):
     """Сериализатор для работ автосервисов и их прайс."""
-    id = serializers.IntegerField(source='job.id', read_only=True)
-    title = serializers.CharField(source='job.title', read_only=True)
+
+    id = serializers.IntegerField(source="job.id", read_only=True)
+    title = serializers.CharField(source="job.title", read_only=True)
 
     class Meta:
         model = AutoserviceJob
         fields = [
-            'id',
-            'title',
-            'price',
+            "id",
+            "title",
+            "price",
         ]
 
 
@@ -93,14 +91,15 @@ class WorkTimeSerializer(serializers.ModelSerializer):
     class Meta:
         model = WorkingTime
         fields = [
-            'id',
-            'day',
-            'time',
+            "id",
+            "day",
+            "time",
         ]
 
 
 class ListAutoServiceSerializer(serializers.ModelSerializer):
     """Сериализатор для списка автосервисов."""
+
     company = CompanyShortSerializer()
     geolocation = GeolocationAutoServiceSerializer()
     rating = serializers.FloatField(read_only=True)
@@ -110,13 +109,13 @@ class ListAutoServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = AutoService
         fields = [
-            'id',
-            'company',
-            'geolocation',
-            'address',
-            'rating',
-            'votes',
-            'working_time_today',
+            "id",
+            "company",
+            "geolocation",
+            "address",
+            "rating",
+            "votes",
+            "working_time_today",
         ]
 
     def get_working_time_today(self, obj: AutoService) -> str:
@@ -136,16 +135,16 @@ class ListAutoServiceSerializer(serializers.ModelSerializer):
         working_days_in_current_autoservices = obj.working_time.all()
         for item in working_days_in_current_autoservices:
             if item.day == settings.NUMBER_WEEK[number_day_of_week]:
-                return f'{item.day}: {item.time}'
+                return f"{item.day}: {item.time}"
 
 
 class AutoServiceSerializer(serializers.ModelSerializer):
     """Сериализатор автосервиса."""
+
     company = CompanySerializer()
     geolocation = GeolocationAutoServiceSerializer()
     city = serializers.SlugRelatedField(
-        slug_field='rus_name',
-        queryset=City.objects.all()
+        slug_field="rus_name", queryset=City.objects.all()
     )
     car_service = TransportsSerializer(many=True)
     working_time = WorkTimeSerializer(many=True)
@@ -156,19 +155,19 @@ class AutoServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = AutoService
         fields = [
-            'id',
-            'company',
-            'geolocation',
-            'city',
-            'address',
-            'rating',
-            'votes',
-            'working_time',
-            'phone_number',
-            'email',
-            'site',
-            'car_service',
-            'job',
+            "id",
+            "company",
+            "geolocation",
+            "city",
+            "address",
+            "rating",
+            "votes",
+            "working_time",
+            "phone_number",
+            "email",
+            "site",
+            "car_service",
+            "job",
         ]
 
     def get_job(self, obj):
@@ -177,55 +176,49 @@ class AutoServiceSerializer(serializers.ModelSerializer):
 
 
 class ImageSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Image
-        fields = ['id', 'image']
+        fields = ["id", "image"]
 
 
 class FeedbackSerializer(serializers.ModelSerializer):
     """Сериализатор для модели Feedback."""
+
     # поле для изображений
     images = ImageSerializer(
-        many=True,
-        required=False,
-        help_text='Загрузите изображение (необязательно)'
+        many=True, required=False, help_text="Загрузите изображение (необязательно)"
     )
     author = serializers.SlugRelatedField(
-        read_only=True,
-        slug_field='email',
-        default=serializers.CurrentUserDefault()
+        read_only=True, slug_field="email", default=serializers.CurrentUserDefault()
     )
 
     def validate(self, data):
-        if self.context['request'].method != 'POST':
+        if self.context["request"].method != "POST":
             return data
         if Feedback.objects.filter(
-            author=self.context['request'].user,
+            author=self.context["request"].user,
             autoservice=get_object_or_404(
-                AutoService,
-                id=self.context['view'].kwargs.get('autoservice_id')
-            )
+                AutoService, id=self.context["view"].kwargs.get("autoservice_id")
+            ),
         ).exists():
-            raise serializers.ValidationError(
-                'Можно оставить только один отзыв'
-            )
+            raise serializers.ValidationError("Можно оставить только один отзыв")
         return data
 
     class Meta:
         model = Feedback
         fields = (
-            'id',
-            'author',
-            'text',
-            'score',
-            'pub_date',
-            'images',
+            "id",
+            "author",
+            "text",
+            "score",
+            "pub_date",
+            "images",
         )
 
 
 class JobsSerializer(serializers.ModelSerializer):
     """Сериализатор для работ автосервиса."""
+
     class Meta:
         model = Job
         fields = "__all__"

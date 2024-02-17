1860378963bd5f1ab06112c1591b49436d6c18d5
GeorgiyKuzora <rafale87@gmail.com>
2024-02-17 13:00:10 +0300

    refactor: format with black /api/v1/users/serializers.py

--- api/v1/users/serializers.py
+++ api/v1/users/serializers.py
@@ -2,8 +2,8 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import Group
 from djoser.serializers import UserSerializer
 from rest_framework import serializers
-from api.v1.autoservice.serializers import CompanyRegistrationSerializer
 
+from api.v1.autoservice.serializers import CompanyRegistrationSerializer
 from users.models import CustomUser
 
 
@@ -11,33 +11,30 @@ class CustomUserSerializer(UserSerializer):
     """
     Сериализатор для модели пользователя CustomUser
     """
-    password = serializers.CharField(
-        style={"input_type": "password"},
-        write_only=True
-    )
+
+    password = serializers.CharField(style={"input_type": "password"}, write_only=True)
 
     class Meta:
         model = CustomUser
-        fields = ('id',
-                  'email',
-                  'first_name',
-                  'password',
-                  'phone_number',
-                  'date_joined'
-                  )
+        fields = (
+            "id",
+            "email",
+            "first_name",
+            "password",
+            "phone_number",
+            "date_joined",
+        )
 
     def create(self, validated_data):
-        validated_data['password'] = make_password(
-            validated_data.get('password'))
+        validated_data["password"] = make_password(validated_data.get("password"))
         return super(UserSerializer, self).create(validated_data)
 
     def update(self, instance, validated_data):
-        validated_data['password'] = make_password(
-            validated_data.get('password'))
+        validated_data["password"] = make_password(validated_data.get("password"))
         return super(UserSerializer, self).update(instance, validated_data)
 
     def add_company_owners_group(self, instance):
-        group, _ = Group.objects.get_or_create(name='company_owners')
+        group, _ = Group.objects.get_or_create(name="company_owners")
         instance.groups.add(group)
         instance.save()
 
@@ -47,30 +44,27 @@ class CustomCurrentUserSerializer(UserSerializer):
     Сериализатор для модели пользователя CustomUser.
     Когда пользователь аутентифицирован.
     """
-    password = serializers.CharField(
-        style={"input_type": "password"},
-        write_only=True
-    )
+
+    password = serializers.CharField(style={"input_type": "password"}, write_only=True)
 
     class Meta:
         model = CustomUser
-        fields = ('id',
-                  'email',
-                  'first_name',
-                  'password',
-                  'phone_number',
-                  'date_joined'
-                  )
+        fields = (
+            "id",
+            "email",
+            "first_name",
+            "password",
+            "phone_number",
+            "date_joined",
+        )
 
     def create(self, validated_data):
-        validated_data['password'] = make_password(
-            validated_data['password'])
+        validated_data["password"] = make_password(validated_data["password"])
         return super(UserSerializer, self).create(validated_data)
 
     def update(self, instance, validated_data):
-        if validated_data.get('password', None) is not None:
-            validated_data['password'] = make_password(
-                validated_data['password'])
+        if validated_data.get("password", None) is not None:
+            validated_data["password"] = make_password(validated_data["password"])
         return super(UserSerializer, self).update(instance, validated_data)
 
 

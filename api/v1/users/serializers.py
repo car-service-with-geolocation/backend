7f980780aa8c6e7d6ec17abd7867c3ce91d4dfb8
GeorgiyKuzora <rafale87@gmail.com>
2024-02-17 12:54:52 +0300

    fix: fix dict password key access in CustomUserSerialiser

--- api/v1/users/serializers.py
+++ api/v1/users/serializers.py
@@ -26,12 +26,12 @@ class CustomUserSerializer(UserSerializer):
 
     def create(self, validated_data):
         validated_data['password'] = make_password(
-            validated_data['password'])
+            validated_data.get('password'))
         return super(UserSerializer, self).create(validated_data)
 
     def update(self, instance, validated_data):
         validated_data['password'] = make_password(
-            validated_data['password'])
+            validated_data.get('password'))
         return super(UserSerializer, self).update(instance, validated_data)
 
 

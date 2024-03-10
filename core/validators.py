e47aa2826e611d745f99630c3a519af19aba800a
GeorgiyKuzora <rafale87@gmail.com>
2024-03-10 18:21:48 +0300

    add hint to validate_all_isdigit

--- core/validators.py
+++ core/validators.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 
-def validate_all_isdigit(value: str):
+def validate_all_isdigit(value: str) -> None:
     if not all((char.isdigit() for char in value)):
         raise ValidationError(
             _("%(value) contains not digit characters"),
